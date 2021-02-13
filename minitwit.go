1b89cb1b685b0d52fd7498bbd130cef5511eb6c1
S3j5b0 <sejsbo@live.dk>
2021-02-13 13:05:04 +0100

    usertimeline should now be displayed

--- minitwit.go
+++ minitwit.go
@@ -225,7 +225,7 @@ func userTimelineHandler(w http.ResponseWriter, r *http.Request) {
 		followed = res.Next() // Checks if the user that is signed in, is currently following the user on the page
 	}
 
-	tmpl, err := template.ParseFiles(timelinePath)
+	tmpl, err := template.ParseFiles(timelinePath, layoutPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -252,7 +252,7 @@ func userTimelineHandler(w http.ResponseWriter, r *http.Request) {
 			err := messagesAndUsers.Scan(&messageID, &authorID, &text, &pubDate, &flagged, &userId, &username, &email, &pwHash)
 			if err == nil {
 				message := Message{
-					Email:    email,
+					Email:    gravatarURL(email, 48),
 					Username: username,
 					Text:     text,
 					PubDate:  formatDatetime(int64(pubDate)),
@@ -263,6 +263,7 @@ func userTimelineHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	data["messages"] = messages
 	data["title"] = fmt.Sprintf("%s's Timeline", profileUsername)
+	data["msgCount"] = len(messages)
 	tmpl.Execute(w, data)
 }
 
