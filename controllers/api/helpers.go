61744ccaad045105e5a629d9f446067df056b3bc
S3j5b0 <sejsbo@live.dk>
2021-03-07 13:18:16 +0100

    #92 updating latest variable for config table

--- controllers/api/helpers.go
+++ controllers/api/helpers.go
@@ -3,11 +3,15 @@ package api
 import (
 	"net/http"
 	"strconv"
+
+	"github.com/heyjoakim/devops-21/models"
+	"github.com/heyjoakim/devops-21/services"
 )
 
-// var (
-// 	latest = 0
-// )
+var (
+	latest = 0
+)
+var d = services.GetDbInstance()
 
 func updateLatest(r *http.Request) {
 	tryLatestQuery := r.URL.Query().Get("latest")
@@ -16,7 +20,21 @@ func updateLatest(r *http.Request) {
 		latest = -1
 	} else {
 		tryLatest, _ := strconv.Atoi(tryLatestQuery)
-		//latest = tryLatest
-		// TODO: make a db.config table, find entry where key name is latest, or first element, and then update the value with tryLatest
+
+		latest = tryLatest
+	}
+
+	var c models.Config
+
+	er := d.DB.First(&c, "key = ?", "latest").Error
+	if er != nil {
+
+		c.ID = 0
+		c.Key = "latest"
+		c.Value = strconv.Itoa(latest)
+		d.DB.Create(&c)
+	} else {
+
+		d.DB.Model(&models.Config{}).Where("key = ?", "latest").Update("Value", latest)
 	}
 }
