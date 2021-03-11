df40c89aab8728982f50e2ff12c37412cdaa4785
Kasper Olsen <root@kols.dk>
2021-03-11 20:08:09 +0100

    added more prometheus metrics

--- metrics/prometheus.go
+++ metrics/prometheus.go
@@ -4,32 +4,76 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/heyjoakim/devops-21/services"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
 // InitializeMetrics incvokes custom metric functions
 func InitializeMetrics() {
 	fmt.Println("Init metrics")
 	cpuMetrics()
-	exampleGauge()
+	memoryMetrics()
+	userCountMetrics()
+	messageCountMetrics()
 }
 
-func exampleGauge() {
-	opsQueued := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "our_company",
-		Subsystem: "blob_storage",
-		Name:      "ops_queued",
-		Help:      "Number of blob storage operations waiting to be processed.",
+func messageCountMetrics() {
+	messagesGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "group_l",
+		Subsystem: "minitwut",
+		Name:      "message_count",
+		Help:      "The current number of messages in the database",
 	})
-	prometheus.MustRegister(opsQueued)
 
-	// 10 operations queued by the goroutine managing incoming requests.
-	opsQueued.Add(10)
-	// A worker goroutine has picked up a waiting operation.
-	opsQueued.Dec()
-	// And once more...
-	opsQueued.Dec()
+	prometheus.MustRegister(messagesGauge)
+
+	go func() {
+		for {
+			count := services.GetMessageCount()
+			messagesGauge.Set(float64(count))
+			time.Sleep(5 * time.Second)
+		}
+	}()
+}
+
+func userCountMetrics() {
+	userGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "group_l",
+		Subsystem: "minitwut",
+		Name:      "user_count",
+		Help:      "The current number of registered users in the database",
+	})
+
+	prometheus.MustRegister(userGauge)
+
+	go func() {
+		for {
+			count := services.GetUserCount()
+			userGauge.Set(float64(count))
+			time.Sleep(5 * time.Second)
+		}
+	}()
+}
+
+func memoryMetrics() {
+	memoryGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "group_l",
+		Subsystem: "minitwut",
+		Name:      "memory_usage",
+		Help:      "The current memory usage for the system",
+	})
+
+	prometheus.MustRegister(memoryGauge)
+
+	go func() {
+		for {
+			v, _ := mem.VirtualMemory()
+			memoryGauge.Set(v.UsedPercent)
+			time.Sleep(5 * time.Second)
+		}
+	}()
 }
 
 func cpuMetrics() {
@@ -47,7 +91,6 @@ func cpuMetrics() {
 			percent, _ := cpu.Percent(0, true)
 			cpuGauge.Set(percent[cpu.CPUser])
 			time.Sleep(5 * time.Second)
-			fmt.Println(percent[cpu.CPUser])
 		}
 	}()
 }
